ce2c727dfbd3f65c38abc2b0ab5634da4bae8331
nicktombeur <nicktombeur@gmail.com>
2019-07-16 22:14:47 +0200

    heroku test

--- src/main/java/info/artificialcolors/core/ArtificialcolorsApplication.java
+++ src/main/java/info/artificialcolors/core/ArtificialcolorsApplication.java
@@ -2,9 +2,19 @@ package info.artificialcolors.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class ArtificialColorsApplication {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    String test() {
+        return "This a test for Heroku!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ArtificialColorsApplication.class, args);
