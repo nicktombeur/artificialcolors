f239fa687e0c3587432339dfbe96a88ab1e3faf8
nicktombeur <nicktombeur@gmail.com>
2019-07-16 22:07:42 +0200

    init

---
+++ src/main/java/info/artificialcolors/core/ArtificialcolorsApplication.java
@@ -0,0 +1,13 @@
+package info.artificialcolors.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtificialColorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtificialColorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/artificialcolors/core/ArtificialcolorsApplicationTests.java
@@ -0,0 +1,16 @@
+package info.artificialcolors.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArtificialColorsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
