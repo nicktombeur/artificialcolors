5f39ed065b84a48ab25c888a08dc2a1f41f4ca2a
nicktombeur <nicktombeur@gmail.com>
2019-07-29 20:37:08 +0200

    code improvement

--- back-end/src/main/java/info/artificialcolors/features/web/WebController.java
+++ back-end/src/main/java/info/artificialcolors/features/web/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebController {
 
     @RequestMapping("/")
-    String index() {
+    public String index() {
         return "index";
     }
 
