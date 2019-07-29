27312e3e79941bd42e3b81aa41e76f0551dc3d91
nicktombeur <nicktombeur@gmail.com>
2019-07-29 20:48:44 +0200

    code improvement

--- back-end/src/main/resources/db/migration/V201907232010__create_insert_translation_procedure.sql
+++ back-end/src/main/resources/db/migration/V201907232010__create_insert_translation_procedure.sql
@@ -1,6 +1,7 @@
-CREATE PROCEDURE insert_translation(number_code VARCHAR(10), language VARCHAR(5), name VARCHAR(255), is_default BOOLEAN DEFAULT FALSE)
-LANGUAGE plpgsql
-AS $$
+CREATE PROCEDURE insert_translation(number_code VARCHAR(10), language VARCHAR(5), name VARCHAR(255),
+                                    is_default BOOLEAN DEFAULT FALSE)
+AS
+$$
 DECLARE
     number_id INTEGER := (SELECT id
                           FROM number
@@ -8,4 +9,5 @@ DECLARE
 BEGIN
     INSERT INTO number_translation VALUES (nextval('number_translation_id_seq'), number_id, language, is_default, name);
 END;
-$$;
+$$
+    LANGUAGE plpgsql;
